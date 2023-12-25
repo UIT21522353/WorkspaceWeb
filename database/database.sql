2e9b99be4edb5f221d798510d88b024d9b7bc714
hgmjkhug <yeuthaoto@gmail.com>
2023-12-25 22:04:35 +0700

    update database

--- database/database.sql
+++ database/database.sql
@@ -29,9 +29,6 @@ SET
 --
 -- Cơ sở dữ liệu: `gameweb`
 --
-CREATE DATABASE IF NOT EXISTS `gameweb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-
-USE `gameweb`;
 
 -- --------------------------------------------------------
 --
